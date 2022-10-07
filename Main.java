e5f666fd48c643191f78545a4754dbb0d8f46ff9
Pablo <108701554+prlainez@users.noreply.github.com>
2022-10-07 00:18:04 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+// you may use this file to wrote and run code to test your MyArrayList class
+
+public class Main {
+   public static void main(String[] args) {
+      
+   }
+}
---
+++ MyLinkedList.java
@@ -0,0 +1,41 @@
+// Complete the implementation of your MyLinkedList class in this file
+
+
+public class MyLinkedList implements MyList {
+    // Implement the required fields and methods here
+    
+    
+   
+	// Do not alter the code below 
+	
+	public MyListIterator getIterator() {
+		return new MyLinkedListIterator();
+	}
+	
+	private class MyLinkedListIterator implements MyListIterator {
+		Node currentNode = null;
+
+		@Override
+		public Object next() {
+			if (currentNode != null)
+				currentNode = currentNode.next;
+			else
+				currentNode = head;
+
+			return currentNode.data;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (currentNode != null)
+				return currentNode.next != null;
+			else
+				return head != null;
+		}
+	}
+	
+	class Node {
+		public Object data = null;
+		public Node next = null;
+	}
+}
